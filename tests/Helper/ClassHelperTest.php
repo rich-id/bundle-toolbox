d0f9cb0ea743c550d289385457bc45570e898dbf
Nicolas Guilloux <nicolas.guilloux@protonmail.com>
2021-08-04 10:38:07 +0200

    Fix tests and CI and directory structure

--- tests/Helper/ClassHelperTest.php
+++ tests/Helper/ClassHelperTest.php
@@ -5,6 +5,7 @@ namespace RichCongress\BundleToolbox\Tests\Helper;
 use PHPUnit\Framework\TestCase;
 use RichCongress\BundleToolbox\Helper\ClassHelper;
 use RichCongress\BundleToolbox\RichCongressBundleToolboxBundle;
+use RichCongress\BundleToolbox\Tests\Flag;
 use RichCongress\BundleToolbox\Tests\Resources\DummyExtension;
 use RichCongress\BundleToolbox\Tests\Resources\PrependConfiguration\BadPrependConfiguration;
 use RichCongress\BundleToolbox\Tests\Resources\PrependConfiguration\DummyDoctrineMigrationPrependConfiguration;
@@ -27,7 +28,7 @@ final class ClassHelperTest extends TestCase
 
     public function testFindClassRelativelyToObjectForComplexePath(): void
     {
-        $found = ClassHelper::findClassRelativelyToObject(RichCongressBundleToolboxBundle::class, 'Tests/Resources/PrependConfiguration');
+        $found = ClassHelper::findClassRelativelyToObject(Flag::class, 'Resources/PrependConfiguration');
 
         self::assertCount(3, $found);
         self::assertContains(BadPrependConfiguration::class, $found);
